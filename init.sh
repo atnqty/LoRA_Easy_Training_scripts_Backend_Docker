e8c2e4dd72a038959f8c05e794ee6195326e75b6
atnqty <49927384+atnqty@users.noreply.github.com>
2026-02-13 16:29:04 -0500

    Add branch variable for git checkout
    Added branch variable to checkout specific branch.

--- init.sh
+++ init.sh
@@ -2,12 +2,14 @@
 echo "Starting Stable Diffusion Trainer"
 
 FORK=${FORK:-derrian-distro}
+BRANCH=${BRANCH:-main}
 
 if [ ! -d "/trainer/$FORK" ] || [ ! "$(ls -A "/trainer/$FORK")" ]; then
   echo "Files not found, cloning..."
   mkdir -p /trainer/$FORK
   git clone https://github.com/$FORK/LoRA_Easy_Training_scripts_Backend.git /trainer/$FORK
   cd /trainer/$FORK
+  git checkout $BRANCH
   python3.10 /trainer/$FORK/installer.py local
   chmod +x /trainer/$FORK/run.sh
   exec /trainer/$FORK/run.sh $ARGS
@@ -15,6 +17,7 @@ else
   echo "Files found, starting..."
   cd /trainer/$FORK
   git pull
+  git checkout $BRANCH
   chmod +x /trainer/$FORK/run.sh
   exec /trainer/$FORK/run.sh $ARGS
 fi
